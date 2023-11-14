b5308ee3f897bbea5263deafc5cb35da7036d6ff
Jesse Kalil <jessekalyl1491@gmail.com>
2023-11-13 21:20:30 -0300

    Update .zshrc and add .spaceshiprc.zsh

---
+++ dot_spaceshiprc.zsh
@@ -0,0 +1,23 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+SPACESHIP_PROMPT_ORDER=(
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  node		# Node.js section	
+  golang	# Golang section
+  exec_time     # Execution time
+  line_sep      # Line break
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL=""
+SPACESHIP_CHAR_SUFFIX=" "
