35fde3e57276add46a7f4da6f9a9db450405bc57
Jesse Kalil <jessekalyl1491@gmail.com>
2024-09-15 14:29:42 -0300

    update configs

--- dot_spaceshiprc.zsh
+++ dot_spaceshiprc.zsh
@@ -5,14 +5,12 @@ SPACESHIP_TIME_SHOW=true
 SPACESHIP_USER_SHOW=always
 
 SPACESHIP_PROMPT_ORDER=(
-  user          # Username section
   dir           # Current directory section
-  host          # Hostname section
   git           # Git section (git_branch + git_status)
   hg            # Mercurial section (hg_branch  + hg_status)
-  node		# Node.js section	
-  golang	# Golang section
-  # exec_time     # Execution time
+  node		      # Node.js section	
+  golang	      # Golang section
+  exec_time     # Execution time
   line_sep      # Line break
   jobs          # Background jobs indicator
   exit_code     # Exit code section
